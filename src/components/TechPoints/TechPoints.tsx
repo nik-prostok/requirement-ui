8c155ca10ab70b282d597ac8df80ad75aa980afc
Nikita Prostokvashin <nikita.umet@gmail.com>
2020-12-03 02:07:32 +0300

    add highlighter

--- src/components/TechPoints/TechPoints.tsx
+++ src/components/TechPoints/TechPoints.tsx
@@ -1,5 +1,57 @@
 import * as React from 'react';
 
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import PDFWorker from "worker-loader!pdfjs-dist/lib/pdf.worker";
+
+import {
+    PdfLoader,
+    PdfHighlighter,
+    Tip,
+    Highlight,
+    Popup,
+    AreaHighlight,
+    setPdfWorker,
+
+} from "react-pdf-highlighter";
+import {useState} from "react";
+import {T_Highlight} from "../../../types/react-pdf-highlighter/types";
+import {Container} from "@material-ui/core";
+
+setPdfWorker(PDFWorker);
+
+interface Created {
+    scrollTo: any;
+}
+
+
 export const TechPoints = () => {
-    return <p>TechPoints</p>
+
+    const [docURL, setDocURL] = useState<string>('/technicalTask/download?technicalTaskId=131');
+    const [highlights, setHighlights] = useState<T_Highlight[]>([]);
+
+    const scrollViewerTo = (highlight: any) => {
+    };
+
+    const getHighlightById = (id: string) => {
+        return highlights.find(highlight => highlight.id === id);
+    }
+
+    return (
+        <div style={{
+            height: "86vh",
+            width: "50vw",
+            position: "relative"
+        }}>
+            <PdfLoader url={docURL}>
+                {(pdfDocument: any) => (
+                    <PdfHighlighter
+                        pdfDocument={pdfDocument}
+                        scrollRef={() => {
+                        }}
+                        highlights={highlights}
+                    />
+                )}
+            </PdfLoader>
+        </div>
+    )
 }
